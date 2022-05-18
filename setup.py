7eae77249c4aa661faa5a7c9c281234a1668ba05
tkpllc <85380202+tkpllc@users.noreply.github.com>
2022-05-18 12:27:23 +0900

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+	name='mytestlib-tkp',
+	version='1.0.0',
+	url='https://hooks.slack.com/services/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',
+	author='tkp',
+	author_email='tkplimitedliabilitycompany@gmail.com',
+	description='My Test Liabrary.',
+	package=find_packages()
+)
